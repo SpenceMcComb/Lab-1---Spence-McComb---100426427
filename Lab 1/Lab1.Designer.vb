b1bc915f33da38975e699497a996554ab0a4d490
Spence <Spence@DESKTOP-GAVTPRF>
2018-03-07 00:00:09 -0500

    Add project files.

---
+++ Lab 1/Lab1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLab1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblUserInput = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.txtUserInput = New System.Windows.Forms.TextBox()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtUnitsShipped = New System.Windows.Forms.TextBox()
+        Me.ToolTips = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblUserInput
+        '
+        Me.lblUserInput.Location = New System.Drawing.Point(64, 21)
+        Me.lblUserInput.Name = "lblUserInput"
+        Me.lblUserInput.Size = New System.Drawing.Size(34, 13)
+        Me.lblUserInput.TabIndex = 0
+        Me.lblUserInput.Text = "&Units:"
+        Me.lblUserInput.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblDay
+        '
+        Me.lblDay.Location = New System.Drawing.Point(217, 21)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(35, 13)
+        Me.lblDay.TabIndex = 0
+        Me.lblDay.Text = "Day 1"
+        Me.lblDay.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtUserInput
+        '
+        Me.txtUserInput.Location = New System.Drawing.Point(104, 18)
+        Me.txtUserInput.Name = "txtUserInput"
+        Me.txtUserInput.Size = New System.Drawing.Size(75, 20)
+        Me.txtUserInput.TabIndex = 1
+        Me.ToolTips.SetToolTip(Me.txtUserInput, "Enter the daily units shipped here.")
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAverage.Location = New System.Drawing.Point(42, 168)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(210, 23)
+        Me.lblAverage.TabIndex = 0
+        Me.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTips.SetToolTip(Me.lblAverage, "The result of the average calculation.")
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(23, 194)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 2
+        Me.btnEnter.Text = "&Enter"
+        Me.ToolTips.SetToolTip(Me.btnEnter, "Calulates the average of the 7 entires.")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(104, 194)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "&Reset"
+        Me.ToolTips.SetToolTip(Me.btnReset, "Resets the form.")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(188, 194)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.ToolTips.SetToolTip(Me.btnExit, "Exits the form.")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtUnitsShipped
+        '
+        Me.txtUnitsShipped.Location = New System.Drawing.Point(42, 44)
+        Me.txtUnitsShipped.Multiline = True
+        Me.txtUnitsShipped.Name = "txtUnitsShipped"
+        Me.txtUnitsShipped.Size = New System.Drawing.Size(210, 121)
+        Me.txtUnitsShipped.TabIndex = 0
+        Me.txtUnitsShipped.TabStop = False
+        Me.ToolTips.SetToolTip(Me.txtUnitsShipped, "Previous days' shipped units.")
+        '
+        'frmLab1
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(292, 224)
+        Me.Controls.Add(Me.txtUnitsShipped)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.txtUserInput)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblUserInput)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmLab1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped Per Week"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUserInput As Label
+    Friend WithEvents lblDay As Label
+    Friend WithEvents txtUserInput As TextBox
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtUnitsShipped As TextBox
+    Friend WithEvents ToolTips As ToolTip
+End Class
---
+++ Lab 1/Lab1.vb
@@ -0,0 +1,106 @@
+﻿
+''' Program Name : Lab 1 - Average Units Shipped
+''' Author: Spence McComb
+''' Student ID: 100426427
+''' Last Edited: January 30, 2018
+''' Course Code: NETD 2202-04
+''' 
+''' Description: The purpose of this program is to accept seven user inputs and calculate an average.
+'''              Afterward, the user has the option to reset or exit the form.
+
+Public Class frmLab1
+
+    'Declare the maximum array size.
+    Const MAXIMUM_ARRAY_SIZE = 6
+
+    'Declare the globally-used variables.
+    Dim entryArray(MAXIMUM_ARRAY_SIZE) As Integer
+    Dim counter As Integer = 0
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Lets the user exit the form by clicking the exit button.
+        Application.Exit()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        'Reset the variables for a new set of data entry.
+        counter = 0
+
+        'Reset the form controls.
+        txtUserInput.Focus()
+        txtUserInput.Text = ""
+        lblDay.Text = "Day 1"
+        txtUnitsShipped.Text = ""
+        lblAverage.Text = ""
+
+        'Enable the data entry textbox and give it the focus.
+        txtUserInput.Enabled = True
+        btnEnter.Enabled = True
+        txtUserInput.Focus()
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        'Declare the locally-used variables.
+        Dim entryAverage As Double
+        Dim isInteger As Integer
+        Dim entrySum As Integer = 0
+
+        'Data validation for blank text and non-numeric values.
+        'This ensures that the data entered by the user can be processed.
+        If (txtUserInput.Text = "") Then
+            MessageBox.Show("No information entered! Please enter a whole number between 0 and 1,000, inclusive.")
+            txtUserInput.SelectAll()
+
+        ElseIf Int32.TryParse(txtUserInput.Text, isInteger) = False Then
+            'Data entered is non-numerical.
+            MessageBox.Show("Invalid entry. Please enter a whole number between 0 and 1,000, inclusive.")
+            txtUserInput.SelectAll()
+
+        ElseIf Int(txtUserInput.Text) > 1000 Or Int(txtUserInput.Text) < 0 Then
+            'Data entered is numerical, but outside of specified range.
+            MessageBox.Show("Entry outside of range. Please enter a whole number between 0 and 1,000, inclusive.")
+            txtUserInput.SelectAll()
+
+        Else
+            'Data is sound. Proceed with the calculations.
+            'Collect user input from the appropriate textbox.
+            entryArray(counter) = txtUserInput.Text
+
+            'Output the collected data to the appropriate textbox.
+            txtUnitsShipped.Text += (entryArray(counter).ToString)
+            txtUnitsShipped.Text &= Environment.NewLine
+
+            'Reset the user input textbox and give it the focus.
+            txtUserInput.Text = ""
+            txtUserInput.Focus()
+
+            'Increment the counter and update the day label.
+            counter = counter + 1
+            lblDay.Text = "Day " + (counter + 1).ToString
+
+            'Perform the average calculation when the counter reaches 7 numbers inputted.
+            'Declare a counter to determine when the stop accepting numbers into the sum.
+            If (counter = 7) Then
+                For i As Integer = 0 To (counter - 1)
+                    entrySum += entryArray(i)
+                Next
+
+                'Ensure the day label does not increment to 8.
+                lblDay.Text = "Day " + (counter).ToString
+
+                'Perform the average calculation and output the result.
+                'Disable the input textbox so that no new numbers can be entered.
+                'Disable the enter button until the user resets the form.
+                entryAverage = (entrySum / counter)
+                lblAverage.Text += "Average per day: " + (Math.Round(entryAverage, 2)).ToString
+                txtUserInput.Enabled = False
+                btnEnter.Enabled = False
+            End If
+        End If
+    End Sub
+
+End Class
